8bf8f03939b2d95e842abd189b175d9eaa9e0216
Brandon Quach <bquach@umich.edu>
2021-10-14 21:51:25 -0400

    Updated Visuals

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const song = document.querySelector(".song");
+document.getElementsByClassName("song").loop = true;
+song.loop = true;
 const play = document.querySelector(".play");
 const replay = document.querySelector(".replay");
 const outline = document.querySelector(".moving-outline circle");
-const video = document.querySelector(".vid video");
 const timeDisplay = document.querySelector(".time-display");
 const outlineLength = outline.getTotalLength();
 const timeSelect = document.querySelectorAll(".time-select button");
+let runCount = 0;
 let Duration = 600;
 
 outline.style.strokeDashoffset = outlineLength;
@@ -23,13 +25,9 @@ play.addEventListener("click", function () {
     if (song.paused) {
         play.src = "./svg/pause.svg";
         song.play();
-        video.play();
-
     } else {
         play.src = "./svg/play.svg";
         song.pause();
-        video.pause();
-
     }
 });
 
@@ -51,23 +49,29 @@ timeSelect.forEach(option => {
 
 song.ontimeupdate = function () {
     let currentTime = song.currentTime;
-    let elapsed = Duration - currentTime;
+    if (currentTime == 0) {
+        runCount++;
+    }
+    let elapsed = Duration - currentTime - (600 * runCount);
     let seconds = Math.floor(elapsed % 60);
     let minutes = Math.floor(elapsed / 60);
     if (seconds == 0) {
         timeDisplay.textContent = `${minutes}:00`;
     }
+    else if (seconds < 10) {
+        timeDisplay.textContent = `${minutes}:0${seconds}`;
+    }
     else {
         timeDisplay.textContent = `${minutes}:${seconds}`;
     }
 
-    let progress = outlineLength - (currentTime / Duration) * outlineLength;
+    let progress = outlineLength - ((currentTime + 600 * runCount) / Duration) * outlineLength;
     outline.style.strokeDashoffset = progress;
 
-    if (currentTime >= Duration) {
+    if (currentTime + 600 * runCount >= Duration) {
         song.pause();
-        song.currentTime = 0;
+        runCount = 0;
+        timeDisplay.textContent = `0:00`;
         play.src = "./svg/play.svg";
-        video.pause();
     }
 };
